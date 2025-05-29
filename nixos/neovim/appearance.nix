e3de24a0fc0cbdd587f9f4ad18767e4680cb4f03
A.Brumsen <a.brumsen@protonmail.com>
2025-05-29 20:31:37 +0200

    Added navbuddy and breadcrumbs

--- nixos/neovim/appearance.nix
+++ nixos/neovim/appearance.nix
@@ -7,11 +7,22 @@
     dashboard.alpha.enable = true;
     ui = {
       noice.enable = true;
+      breadcrumbs = {
+        enable = true;
+        navbuddy.enable = true;
+      };
       illuminate.enable = true;
       colorizer.enable = true;
     };
     visuals.indent-blankline.enable = true;
     statusline.lualine.enable = true;
     tabline.nvimBufferline.enable = true;
+    keymaps = [
+      {
+        key = "<leader>nv";
+        mode = "n";
+        action = ":Navbuddy<CR>";
+      }
+    ];
   };
 }
